018da4914c5b79d9fab72be18ccd115e0f8c22ee
e0 <shan.huang@me.com>
2023-02-20 20:41:24 +0100

    chore: include types for the build output

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,13 @@ export default defineConfig({
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
 			name: 'base-report-util',
-			fileName: 'base-report-util'
+			fileName: 'base-report-util',
+			formats: ['es']
 		}
 	},
-	plugins: [dts()]
+	plugins: [
+		dts({
+			insertTypesEntry: true
+		})
+	]
 })
