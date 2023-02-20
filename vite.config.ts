ff448e46137f3c82642cc20dab648e8d7cf9a9c4
e0 <shan.huang@me.com>
2023-02-20 23:55:10 +0100

    feat: use ncc

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,3 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/guide/build.html#library-mode
-export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(__dirname, 'src/index.ts'),
-			name: 'base-report-util',
-			fileName: 'base-report-util',
-			formats: ['es']
-		}
-	},
-	plugins: [
-		dts({
-			insertTypesEntry: true
-		})
-	]
-})
+export default defineConfig({})
